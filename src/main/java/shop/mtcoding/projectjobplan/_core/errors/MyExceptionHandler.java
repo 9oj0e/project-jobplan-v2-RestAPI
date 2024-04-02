42fd65454e3da3681efdc304dcba6c2f165706ca
David Jo <126438339+9oj0e@users.noreply.github.com>
2024-04-02 16:36:38 +0900

    Update MyExceptionHandler.java
    a typo found and fixed

--- src/main/java/shop/mtcoding/projectjobplan/_core/errors/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/projectjobplan/_core/errors/MyExceptionHandler.java
@@ -9,26 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 import shop.mtcoding.blog._core.err.exception.*;
 import shop.mtcoding.blog._core.util.ApiUtil;
 
-@RestController //runtimeException 이 터지만 해당 파일로 오류가 모인다.
+@RestControllerAdivce //runtimeException 이 터지만 해당 파일로 오류가 모인다.
 public class MyExeptionHandler{
 
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> ex400(RuntimeException e){
-
         ApiUtil<?> apiUtil = new ApiUtil<>(400,e.getMessage()); // http body 내부의 구성한 객체
+        
         return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http header , http body
     }
+    
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> ex401(RuntimeException e){
         ApiUtil<?> apiUtil = new ApiUtil<>(401,e.getMessage()); // http body 내부의 구성한 객체
+        
         return new ResponseEntity<>(apiUtil,HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> ex403(RuntimeException e){
         ApiUtil<?> apiUtil = new ApiUtil<>(403,e.getMessage()); // http body 내부의 구성한 객체
+        
         return new ResponseEntity<>(apiUtil,HttpStatus.FORBIDDEN);
     }
+    
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> ex404(RuntimeException e){
         ApiUtil<?> apiUtil = new ApiUtil<>(404,e.getMessage()); // http body 내부의 구성한 객체
@@ -39,8 +43,7 @@ public class MyExeptionHandler{
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> ex500(RuntimeException e){
         ApiUtil<?> apiUtil = new ApiUtil<>(500,e.getMessage()); // http body 내부의 구성한 객체
+        
         return new ResponseEntity<>(apiUtil,HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-
 }
