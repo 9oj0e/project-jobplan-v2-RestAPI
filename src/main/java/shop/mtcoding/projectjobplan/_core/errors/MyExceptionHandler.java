021ae37c8be4573c47a1deb16d36a098f217becf
coderyu5523 <153602328+coderyu5523@users.noreply.github.com>
2024-04-02 16:37:47 +0900

    board resume RestAPI (#1)
    * RestAPI + Board & Resume
    
    * Update MyExceptionHandler.java
    
    a typo found and fixed
    
    ---------
    
    Co-authored-by: David Jo <126438339+9oj0e@users.noreply.github.com>

--- src/main/java/shop/mtcoding/projectjobplan/_core/errors/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/projectjobplan/_core/errors/MyExceptionHandler.java
@@ -1,47 +1,49 @@
 package shop.mtcoding.projectjobplan._core.errors;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import shop.mtcoding.projectjobplan._core.errors.exception.*;
-import shop.mtcoding.projectjobplan._core.utils.ApiUtil;
+import org.springframework.web.bind.annotation.RestController;
+import shop.mtcoding.blog._core.err.exception.*;
+import shop.mtcoding.blog._core.util.ApiUtil;
 
-@RestControllerAdvice
-public class MyExceptionHandler {
+@RestControllerAdivce //runtimeException 이 터지만 해당 파일로 오류가 모인다.
+public class MyExeptionHandler{
 
     @ExceptionHandler(Exception400.class)
-    public ResponseEntity<?> ex400(Exception400 e) {
-        ApiUtil<?> apiUtil = new ApiUtil<>(400, e.getMessage()); // http body -> 구성한 객체
-
-        return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http body, http header
+    public ResponseEntity<?> ex400(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(400,e.getMessage()); // http body 내부의 구성한 객체
+        
+        return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http header , http body
     }
-
+    
     @ExceptionHandler(Exception401.class)
-    public ResponseEntity<?> ex401(Exception401 e) {
-        ApiUtil<?> apiUtil = new ApiUtil<>(401, e.getMessage()); // http body -> 구성한 객체
-
-        return new ResponseEntity<>(apiUtil, HttpStatus.UNAUTHORIZED); // http body, http header
+    public ResponseEntity<?> ex401(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(401,e.getMessage()); // http body 내부의 구성한 객체
+        
+        return new ResponseEntity<>(apiUtil,HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception403.class)
-    public ResponseEntity<?> ex403(Exception401 e) {
-        ApiUtil<?> apiUtil = new ApiUtil<>(403, e.getMessage()); // http body -> 구성한 객체
-
-        return new ResponseEntity<>(apiUtil, HttpStatus.FORBIDDEN); // http body, http header
+    public ResponseEntity<?> ex403(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(403,e.getMessage()); // http body 내부의 구성한 객체
+        
+        return new ResponseEntity<>(apiUtil,HttpStatus.FORBIDDEN);
     }
-
+    
     @ExceptionHandler(Exception404.class)
-    public ResponseEntity<?> ex404(Exception401 e) {
-        ApiUtil<?> apiUtil = new ApiUtil<>(404, e.getMessage()); // http body -> 구성한 객체
+    public ResponseEntity<?> ex404(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(404,e.getMessage()); // http body 내부의 구성한 객체
 
-        return new ResponseEntity<>(apiUtil, HttpStatus.NOT_FOUND); // http body, http header
+        return new ResponseEntity<>(apiUtil,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception500.class)
-    public ResponseEntity<?> ex500(Exception401 e) {
-        ApiUtil<?> apiUtil = new ApiUtil<>(500, e.getMessage()); // http body -> 구성한 객체
-
-        return new ResponseEntity<>(apiUtil, HttpStatus.INTERNAL_SERVER_ERROR); // http body, http header
+    public ResponseEntity<?> ex500(RuntimeException e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(500,e.getMessage()); // http body 내부의 구성한 객체
+        
+        return new ResponseEntity<>(apiUtil,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
