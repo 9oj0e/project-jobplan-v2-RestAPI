e0db58dd843877e6e3b16c7078433d89134cc473
yunchocopie <yunnn687@naver.com>
2024-04-03 15:11:44 +0900

    구독 평점 JWT 변경 완료

--- src/main/java/shop/mtcoding/projectjobplan/rating/RatingService.java
+++ src/main/java/shop/mtcoding/projectjobplan/rating/RatingService.java
@@ -3,32 +3,40 @@ package shop.mtcoding.projectjobplan.rating;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.projectjobplan._core.errors.exception.Exception404;
 import shop.mtcoding.projectjobplan.board.Board;
 import shop.mtcoding.projectjobplan.board.BoardJpaRepository;
 import shop.mtcoding.projectjobplan.resume.Resume;
 import shop.mtcoding.projectjobplan.resume.ResumeJpaRepository;
+import shop.mtcoding.projectjobplan.user.SessionUser;
 import shop.mtcoding.projectjobplan.user.User;
+import shop.mtcoding.projectjobplan.user.UserJpaRepository;
 
 @RequiredArgsConstructor
 @Service
 public class RatingService {
+    private final UserJpaRepository userJpaRepository;
     private final RatingJpaRepository ratingJpaRepository;
     private final BoardJpaRepository boardJpaRepository;
     private final ResumeJpaRepository resumeJpaRepository;
 
     @Transactional // 공고 주인 평가
-    public RatingResponse.DTO createRating(User sessionUser, RatingRequest.RateBoardUser requestDTO) {
+    public RatingResponse.DTO createRating(SessionUser sessionUser, RatingRequest.RateBoardUser requestDTO) {
+        User user = userJpaRepository.findById(sessionUser.getId())
+                .orElseThrow(() -> new Exception404("존재하지 않는 유저입니다."));
         Board board = boardJpaRepository.findById(requestDTO.getBoardId()).get();
-        Rating rating = new Rating(sessionUser, board, requestDTO.getRating());
+        Rating rating = new Rating(user, board, requestDTO.getRating());
         Rating result = ratingJpaRepository.save(rating);
 
         return new RatingResponse.DTO(result);
     }
 
     @Transactional // 이력서 주인 평가
-    public RatingResponse.DTO createRating(User sessionUser, RatingRequest.RateResumeUser requestDTO) {
+    public RatingResponse.DTO createRating(SessionUser sessionUser, RatingRequest.RateResumeUser requestDTO) {
+        User user = userJpaRepository.findById(sessionUser.getId())
+                .orElseThrow(() -> new Exception404("존재하지 않는 유저입니다."));
         Resume resume = resumeJpaRepository.findById(requestDTO.getResumeId()).get();
-        Rating rating = new Rating(sessionUser, resume, requestDTO.getRating());
+        Rating rating = new Rating(user, resume, requestDTO.getRating());
         Rating result = ratingJpaRepository.save(rating);
 
         return new RatingResponse.DTO(result);
